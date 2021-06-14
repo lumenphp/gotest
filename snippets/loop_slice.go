ff3837d326a8d23e111d4beef3dd82da61caff19
Gopher <Gopher>
2021-06-13 17:42:32 -0700

    string split

--- snippets/loop_slice.go
+++ snippets/loop_slice.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main(){
-
+	s := "The quick brown fox jumps over the lazy dog"
+	sl := strings.Split(s," ")
+	for _, i := range {
+		fmt.Println(i)
+	}
 }
